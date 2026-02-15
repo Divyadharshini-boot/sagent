4edfe3876cbffeda1e0be8ad9a233fe61636b789
Divyadharshini-boot <divyadharshini508205@gmail.com>
2026-02-15 12:09:03 +0530

    Delete Spring-boot Implementation/College_Admission directory

--- Spring-boot Implementation/College_Admission/ApplicationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.college.admissions.repository;
-
-import com.college.admissions.entity.Application;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationRepository extends JpaRepository<Application, Integer> {
-}
--- Spring-boot Implementation/College_Admission/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.college.admissions.repository;
-
-import com.college.admissions.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Integer> {
-}
